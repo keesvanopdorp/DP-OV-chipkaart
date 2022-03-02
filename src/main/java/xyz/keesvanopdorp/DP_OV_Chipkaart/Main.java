91fc57e0165d4e45937ddc0c6be63df526cadb83
Kees van Opdorp <kvopdorp@mprise.nl>
2022-03-02 22:54:22 +0100

    added test function to opdracht 2

--- src/main/java/xyz/keesvanopdorp/DP_OV_Chipkaart/Main.java
+++ src/main/java/xyz/keesvanopdorp/DP_OV_Chipkaart/Main.java
@@ -1,7 +1,10 @@
 package xyz.keesvanopdorp.DP_OV_Chipkaart;
+import xyz.keesvanopdorp.DP_OV_Chipkaart.dao.ReizigerDAO;
 import xyz.keesvanopdorp.DP_OV_Chipkaart.dao.ReizigerDaoPostgres;
+import xyz.keesvanopdorp.DP_OV_Chipkaart.domain.Reiziger;
 
 import java.sql.*;
+import java.util.List;
 import java.util.Properties;
 
 public class Main {
@@ -55,9 +58,43 @@ public class Main {
                 System.out.printf("#%s %s (%s)%n", rs.getInt("reiziger_id"), name, rs.getString("geboortedatum"));
             }
             System.out.println(new ReizigerDaoPostgres(conn).findAll());
+            testReizigerDAO(new ReizigerDaoPostgres(conn));
         } catch (SQLException e) {
             e.printStackTrace();
         }
         closeConnection();
+    }
+
+    /**
+     * P2. Reiziger DAO: persistentie van een klasse
+     *
+     * Deze methode test de CRUD-functionaliteit van de Reiziger DAO
+     *
+     * @throws SQLException
+     */
+    private static void testReizigerDAO(ReizigerDAO rdao) throws SQLException {
+        System.out.println("\n---------- Test ReizigerDAO -------------");
+
+        // Haal alle reizigers op uit de database
+        List<Reiziger> reizigers = rdao.findAll();
+        System.out.println("[Test] ReizigerDAO.findAll() geeft de volgende reizigers:");
+        for (Reiziger r : reizigers) {
+            System.out.println(r);
+        }
+        System.out.println();
+
+        // Maak een nieuwe reiziger aan en persisteer deze in de database
+        String gbdatum = "1981-03-14";
+        Reiziger sietske = new Reiziger();
+        sietske.setAchternaam("Broers");
+        sietske.setGeboortedatum(Date.valueOf(gbdatum));
+        sietske.setVoorletters("S");
+        sietske.setTussenvoegsel("");
+        System.out.print("[Test] Eerst " + reizigers.size() + " reizigers, na ReizigerDAO.save() ");
+        rdao.save(sietske);
+        reizigers = rdao.findAll();
+        System.out.println(reizigers.size() + " reizigers\n");
+
+        // Voeg aanvullende tests van de ontbrekende CRUD-operaties in.
     }
 }
